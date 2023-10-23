c4654d52211ae0cf8eeee3c6e90d51b0943288c3
Sarvang Dave <sarvang00@gmail.com>
2023-10-22 23:04:57 -0300

    Getting started and HTTP routing

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,12 +5,37 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello from this site!")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Hello from this site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page!</h1>")
+	fmt.Fprint(w, "<p>To get in touch, Email: <a href=\"mailto:sdave@dal.ca\">sdave@dal.ca</a></p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintln(w, r.URL.Path)
+	// fmt.Fprintln(w, r.URL.RawPath)
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// Page not found error
+		// http.NotFound(w, r)
+		http.Error(w, "Page not found", http.StatusNotFound)
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "Page not found")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
