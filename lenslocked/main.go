08e74203882e18c0dc4e10ad8687d6ef042441c5
Sarvang Dave <sarvang00@gmail.com>
2023-12-26 17:37:33 -0400

    Implementing Handler type for custom router

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -16,26 +16,40 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>To get in touch, Email: <a href=\"mailto:sdave@dal.ca\">sdave@dal.ca</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintln(w, r.URL.Path)
-	// fmt.Fprintln(w, r.URL.RawPath)
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprintln(w, r.URL.Path)
+// 	// fmt.Fprintln(w, r.URL.RawPath)
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		// Page not found error
+// 		// http.NotFound(w, r)
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 		// w.WriteHeader(http.StatusNotFound)
+// 		// fmt.Fprint(w, "Page not found")
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		// Page not found error
-		// http.NotFound(w, r)
 		http.Error(w, "Page not found", http.StatusNotFound)
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprint(w, "Page not found")
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/contact", contactHandler)
+	var customRouter Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", customRouter)
 }
