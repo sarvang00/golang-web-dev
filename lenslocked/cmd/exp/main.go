2ca7a5a82def428dbce421e1432dddc536cfc444
Sarvang Dave <sarvang00@gmail.com>
2023-12-30 17:49:28 -0400

    Templating basics

--- lenslocked/cmd/exp/main.go
+++ lenslocked/cmd/exp/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 )
+
+type UserMeta struct {
+	Visits int
+}
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+	Bio1 string
+	Bio2 template.HTML
+}
 
 func main() {
 	t, err := template.ParseFiles("hello.gohtml")
@@ -11,11 +24,17 @@ func main() {
 		panic(err)
 	}
 
-	user := struct {
-		Name string
-	}{
+	user := User{
 		Name: "John Doe",
+		Age:  12,
+		Meta: UserMeta{
+			Visits: 3,
+		},
+		Bio1: `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+		Bio2: `<script>alert("Haha, you have been h4x0r3d!");</script>`,
 	}
+
+	fmt.Println("User meta visits: ", user.Meta.Visits)
 
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
